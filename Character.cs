72572d3944e6bf2509dd276fa22fff877c6d4038
aivanov1337 <42995537+aivanov1337@users.noreply.github.com>
2020-10-07 15:38:19 +0200

    Add files via upload

---
+++ Character.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Xml;
+using System.Xml.Serialization;
+using System;
+
+[Serializable]
+public class Character
+{
+    public string Name;
+    public float Strength;
+    public float Intelligence;
+    public float Agility;
+    public float Armor;
+    public float Critical;
+    public List<string> items = new List<string>();    
+}
---
+++ CharacterLoader.cs
@@ -0,0 +1,50 @@
+﻿using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class CharacterLoader : MonoBehaviour
+{
+    private string path;
+    public List<Character> charDb = new List<Character>();
+    private string file = "characters.json";
+
+    public void Start()
+    {    
+        path = Application.dataPath + "/" + file;        
+        Load();
+    }
+
+    private void Load()
+    {        
+        string json = ReadFromFile(path);
+        JsonUtility.FromJsonOverwrite(json,this);
+    }
+
+    private void Save()
+    {
+        string json = JsonUtility.ToJson(this);
+        WriteToFile(path, json);
+    }
+
+    private void WriteToFile(string path, string json)
+    {
+        FileStream fileStream = new FileStream(path, FileMode.Create);
+
+        using (StreamWriter writer = new StreamWriter(fileStream))
+        {
+            writer.Write(json);
+        }
+    }
+
+    private string ReadFromFile(string path)
+    {
+        using (StreamReader reader = new StreamReader(path))
+        {
+            string json = reader.ReadToEnd();
+            return json;
+        }
+    }
+
+}
